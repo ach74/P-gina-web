c72b0c48d2470da457d8deb0aa1bd665ff7faede
Ismael <32370252+IsmaFuentes@users.noreply.github.com>
2018-04-12 14:09:07 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -3,21 +3,16 @@
 $(document).ready(function(){
     //Llamamos a las funciones//
     $("#boton1").click(function(){goToInicio();});
+    $("#boton2").click(function(){goToNews1();});
 });
 
 
-//botón con funcion de enlace//
+//botones con funcion de enlace//
 function goToInicio(){
 	window.location.href = 'index.html';
 }
 
-/*
-window.onload = function(){
-	document.getElementById("boton1").onclick=function(){inicio()};
+function goToNews1(){
+	window.location.href = 'news1.html';
 }
 
-//botón con funcion de enlace
-function inicio() {
-	location.href = "index.html";
-};
-*/
