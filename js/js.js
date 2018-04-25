9d146e4a62dd7eb5cefb65a7b5bdbd2fdb089a4a
Ismael <32370252+IsmaFuentes@users.noreply.github.com>
2018-04-25 17:20:22 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,13 +1,13 @@
 //Al cargar la página//
-
 $(document).ready(function(){
     //Llamamos a las funciones//
     $("#boton1").click(function(){goToInicio();});
 
     $("#boton2").click(function(){goToNews1();});
 
-});
+    $("#btncargar").click(function(){cargar();});
 
+});
 
 //botones con funcion de enlace//
 function goToInicio(){
@@ -20,3 +20,42 @@ function goToNews1(){
 
 
 
+
+//Carga de archivos JSON
+var numjson = 1;
+
+//Cargar al hacer scroll
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height()) {
+        if (numjson < 4) {
+            $.getJSON("https://cdn.rawgit.com/IsmaFuentes/P-gina-web/f1596aed/json/" + numjson + ".json", function (jsonObject) {
+                addrow(jsonObject);
+            }); numjson++;
+        } 
+    }
+});
+
+//Cargar al usar el boton "More"
+function cargar() {
+    if (numjson < 4) {
+        $.getJSON("https://cdn.rawgit.com/IsmaFuentes/P-gina-web/f1596aed/json/" + numjson + ".json", function (jsonObject) {
+            addrow(jsonObject);
+        }); numjson++;
+    }
+};
+
+//CAMBIAR ENLACES A RAWGIT!!
+//https://rawgit.com/IsmaFuentes/P-gina-web/master/json/
+
+//Presentación
+function addrow(json) {
+    $.each(json, function (i, item) {
+        $("#modelos").append('<div class="col-sm-6 col-md-6">' +
+            '<div class="thumbnail">' +
+            '<div class="caption">' + '<h3 class="text-center">' + item.titulo + "</h3>" + "</div>" +
+            '<img src="' + item.img + '" alt="..." />' +
+            '<div class="caption">' + '<p class="text-justify">' + item.descripcion + "</p>" +
+            '<button type="button" class="btn center-block">View</button' + 
+            "</div>" + "</div>" + "</div>");
+    })
+};
