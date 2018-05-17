46877552c20351f12d713343213c7c4f1f16a107
Ismael <32370252+IsmaFuentes@users.noreply.github.com>
2018-05-17 18:26:48 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,11 +1,19 @@
 //Al cargar la página//
 $(document).ready(function(){
-    //Llamamos a las funciones//
+    //Botones//
     $("#boton1").click(function(){goToInicio();});
 
-    $("#boton2").click(function(){goToNews1();});
+    $("#news1").click(function(){goToNews1();});
+
+    $("#news2").click(function(){goToNews2();});
 
     $("#btncargar").click(function(){cargar();});
+
+    //Escondemos el div con el loading button//
+    $('#loading').hide();
+
+    //botón activar - desactivar scroll
+    $('#autoscroll').click(function(){activateScroll();});
 
 });
 
@@ -18,44 +26,70 @@ function goToNews1(){
 	window.location.href = 'news1.html';
 }
 
+function goToNews2(){
+    window.location.href = 'news2.html';
+}
+//Activar - Desactivar scroll//
+var scrolling = true;
 
+function activateScroll(){
+    if (scrolling){
+        scrolling = false;
+        $('#autoscroll').text('Scroll: OFF');
+    }else {
+        scrolling = true;
+        $('#autoscroll').text('Scroll: ON ');  
+    }
+}
 
-
-//Carga de archivos JSON
+//Carga de archivos JSON//
 var numjson = 1;
 
-//Cargar al hacer scroll
+//Cargar al hacer scroll//
 $(window).scroll(function () {
-    if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height()) {
-        if (numjson < 4) {
-            $.getJSON("https://rawgit.com/IsmaFuentes/P-gina-web/master/json/" + numjson + ".json", function (jsonObject) {
-                addrow(jsonObject);
-            }); numjson++;
-        } 
+    if (scrolling) {
+        if($(window).scrollTop() + $(window).height() + 5 >= $(document).height()) {
+            if (numjson < 4) {
+                //loading div show
+                $("#loading").fadeIn();
+                //carga json
+                $.getJSON("https://rawgit.com/IsmaFuentes/P-gina-web/master/json/" + numjson + ".json", function (jsonObject) {
+                    buildrow(jsonObject);
+                    //loading div hide
+                    $("#loading").fadeOut();
+                }); numjson++;
+            }
+        }
     }
 });
 
-//Cargar al usar el boton "More"
+//Cargar al usar el boton "More"//
 function cargar() {
     if (numjson < 4) {
+        //loading div show
+        $("#loading").fadeIn();
+        //carga json
         $.getJSON("https://rawgit.com/IsmaFuentes/P-gina-web/master/json/" + numjson + ".json", function (jsonObject) {
-            addrow(jsonObject);
+            buildrow(jsonObject);
+            //loading div hide
+            $("#loading").fadeOut();
         }); numjson++;
     }
 };
 
 
-//Presentación
-function addrow(json) {
+//Presentación//
+function buildrow(json) {
     $.each(json, function (i, item) {
         $("#noticias").append('<div class="col-sm-6 col-md-6">' +
             '<div class="thumbnail">' +
             '<div class="caption">' + '<h3 class="text-center">' + item.titulo + "</h3>" + "</div>" +
             '<img src="' + item.img + '" alt="..." />' +
             '<div class="caption">' + '<p class="text-justify">' + item.descripcion + "</p>" +
-            '<button type="button" class="btn center-block">View</button' + 
+            '<button type="button" class="btn center-block">Price</button' + 
             "</div>" + "</div>" + "</div>");
     })
 };
+
 
 
